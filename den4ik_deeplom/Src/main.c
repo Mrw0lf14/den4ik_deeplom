025bd0ca0f404301b4773cd641771c7942619bae
Alexei <mstrwlf7@gmail.com>
2024-12-15 12:00:10 +0300

    [+] всё работает

--- den4ik_deeplom/Src/main.c
+++ den4ik_deeplom/Src/main.c
@@ -4,7 +4,8 @@
 char RxBuffer[RX_BUFF_SIZE];					//Буфер приёма USART
 char TxBuffer[TX_BUFF_SIZE];					//Буфер передачи USART
 bool ComReceived;
-
+int32_t rotation = 0;
+uint32_t MAX_ROTATION = 1000;
 void initClk(void)
 {
 	// Enable HSI
@@ -57,12 +58,12 @@ void initUSART2(void)
 	Скорость передачи данных - 9600
 	Частота шины APB1 - 32МГц
 
-	1. USARTDIV = 32'000'000/(16*115200) = 17.36
-	2. 17 = 0x11
-	3. 16*0.4 = 6
-	4. Итого 0x116
+	1. USARTDIV = 32'000'000/(16*9600) = 208.3
+	2. 208 = 0xD0
+	3. 16*0.3 = 4.8
+	4. Итого 0xD05
 	*****************************************/
-	USART2->BRR = 0x116;
+	USART2->BRR = 0xD05;
 
 	USART2->CR1 |= USART_CR1_RE | USART_CR1_TE | USART_CR1_UE;
 	USART2->CR1 |= USART_CR1_RXNEIE;						//разрешить прерывание по приему байта данных
@@ -104,6 +105,24 @@ void initTIM3_PWM(void)
 	NVIC_SetPriority(TIM3_IRQn, 1);			//Выставляем приоритет
 }
 
+void closeWindow(void)
+{
+	EN1_ON();
+	EN2_OFF();
+	TIM3->CR1 |= TIM_CR1_CEN;
+}
+void openWindow(void)
+{
+	EN1_OFF();
+	EN2_ON();
+	TIM3->CR1 |= TIM_CR1_CEN;
+}
+void stopWindow(void)
+{
+	EN1_OFF();
+	EN2_OFF();
+	TIM3->CR1 &= ~TIM_CR1_CEN;
+}
 void ExecuteCommand(void)
 {
 //	txStr(RxBuffer, false);
@@ -113,32 +132,44 @@ void ExecuteCommand(void)
 	if (strncmp(RxBuffer,"*IDN?",5) == 0)					//Это команда "*IDN?"
 	{
 		//Она самая, возвращаем строку идентификации
-		txStr(TxBuffer);
+		strcpy(TxBuffer, "Denis Chernishev, IU4-31M");
 	}
-	else if (strncmp(RxBuffer,"START",5) == 0)				//Команда запуска таймера?
+	else if (strncmp(RxBuffer,"OPEN",4) == 0)				//Команда запуска таймера?
 	{
-		TIM2->CR1 |= TIM_CR1_CEN;
+
+		strcpy(TxBuffer, "OK");
+	}
+	else if (strncmp(RxBuffer,"CLOSE",5) == 0)				//Команда запуска таймера?
+	{
+
 		strcpy(TxBuffer, "OK");
 	}
 	else if (strncmp(RxBuffer,"STOP",4) == 0)				//Команда остановки таймера?
 	{
-		TIM2->CR1 &= ~TIM_CR1_CEN;
+		stopWindow();
 		strcpy(TxBuffer, "OK");
 	}
-	else if (strncmp(RxBuffer,"PERIOD",6) == 0)				//Команда изменения периода таймера?
+	else if (strncmp(RxBuffer,"SPEED",5) == 0)				//Команда изменения периода таймера?
 	{
 		uint16_t tim_value;
 		sscanf(RxBuffer,"%*s %hu", &tim_value);				//преобразуем строку в целое число
 
-		if ((100 <= tim_value) && (tim_value <= 5000))		//параметр должен быть в заданных пределах!
+		if ((0 <= tim_value) && (tim_value < 320))		//параметр должен быть в заданных пределах!
 		{
-			TIM2->ARR = tim_value;
+			TIM2->CCR1 = tim_value;
 			TIM2->CNT = 0;
 
 			strcpy(TxBuffer, "OK");
 		}
 		else
 			strcpy(TxBuffer, "Parameter is out of range");	//ругаемся
+	}
+	else if (strncmp(RxBuffer,"CALIB",5) == 0)
+	{
+		uint16_t calib;
+		sscanf(RxBuffer,"%*s %hu", &calib);
+		MAX_ROTATION = calib;
+		strcpy(TxBuffer, "OK");
 	}
 	else
 		strcpy(TxBuffer,"Invalid Command");					//Если мы не знаем, чего от нас хотят, ругаемся в ответ
@@ -154,6 +185,8 @@ void initGPIO()
 {
 	RCC->APB2ENR |= RCC_APB2ENR_IOPAEN;			//включить тактирование GPIOA
 	RCC->APB2ENR |= RCC_APB2ENR_AFIOEN;
+	RCC->APB2ENR |= RCC_APB2ENR_IOPCEN;
+	RCC->APB2ENR |= RCC_APB2ENR_IOPBEN;
 
 	//очистка полей
 	GPIOA->CRL &= ~(GPIO_CRL_CNF5 | GPIO_CRL_MODE5);
@@ -165,32 +198,49 @@ void initGPIO()
 	GPIOA->CRH &= ~(GPIO_CRH_CNF9 | GPIO_CRH_MODE9);
 	GPIOA->CRH |= GPIO_CRH_MODE9_1;				//PA9, выход 2МГц
 
+
+
 	GPIOA->CRH &= ~(GPIO_CRH_MODE11 | GPIO_CRH_CNF11);
 	GPIOA->CRH |= GPIO_CRH_CNF11_1;			//Вход с подтяжкой
 	GPIOA->BSRR |= GPIO_BSRR_BS11;			//Подтяжка к Vdd
+	//кнопка на плате
+	GPIOC->CRH &= ~(GPIO_CRH_MODE13 | GPIO_CRH_CNF13);
+	GPIOC->CRH |= GPIO_CRH_CNF13_1;			//Вход с подтяжкой
+	GPIOC->BSRR |= GPIO_BSRR_BS13;			//Подтяжка к Vdd
 
-	GPIOA->CRH &= ~(GPIO_CRH_MODE14 | GPIO_CRH_CNF14);
-	GPIOA->CRH |= GPIO_CRH_CNF14_1;			//Вход с подтяжкой
+	GPIOB->CRH &= ~(GPIO_CRH_MODE14 | GPIO_CRH_CNF14);
+	GPIOB->CRH |= GPIO_CRH_CNF14_1;			//Вход с подтяжкой
+//	GPIOB->BSRR |= GPIO_BSRR_BS14;			//Подтяжка к Vdd
 
-	GPIOA->CRH &= ~(GPIO_CRH_MODE15 | GPIO_CRH_CNF15);
-	GPIOA->CRH |= GPIO_CRH_CNF15_1;			//Вход с подтяжкой
+	GPIOB->CRH &= ~(GPIO_CRH_MODE15 | GPIO_CRH_CNF15);
+	GPIOB->CRH |= GPIO_CRH_CNF15_1;			//Вход с подтяжкой
+//	GPIOB->BSRR |= GPIO_BSRR_BS15;			//Подтяжка к Vdd
 	/* Настройка самого прерывания */
 
 	// Настройка альтернативных фукнций портов.
 	// Настройки портов с 12 по 15 хранятся в регистре AFIO_EXTICR4.
 	// Регистры объединены в массив AFIO->EXTICR, нумерация массива начинается с нулевого элемента.
 	// Поэтому настройки AFIO_EXTICR4 хранятся в AFIO->EXTICR[3]
-	AFIO->EXTICR[2] |= AFIO_EXTICR3_EXTI11_PC;
-	AFIO->EXTICR[3] |= AFIO_EXTICR4_EXTI14_PC;
-	AFIO->EXTICR[3] |= AFIO_EXTICR4_EXTI15_PC;
+	AFIO->EXTICR[2] |= AFIO_EXTICR3_EXTI11_PA;
+	AFIO->EXTICR[3] |= AFIO_EXTICR4_EXTI13_PC;	//кнопка на плате
+	AFIO->EXTICR[3] |= AFIO_EXTICR4_EXTI14_PB;
+	AFIO->EXTICR[3] |= AFIO_EXTICR4_EXTI15_PB;
 
-	EXTI->FTSR |= EXTI_FTSR_TR11 | EXTI_FTSR_TR14 | EXTI_FTSR_TR15;			//Прерывание по спаду импульса (при нажатии на кнопку)
-	EXTI->IMR |= EXTI_IMR_MR11 | EXTI_IMR_MR14 | EXTI_IMR_MR15;				//Выставляем маску - EXTI13
+	EXTI->FTSR |= EXTI_FTSR_TR13;
+	EXTI->IMR |= EXTI_IMR_MR13;
+
+	EXTI->FTSR |= EXTI_FTSR_TR11;
+	EXTI->IMR |= EXTI_IMR_MR11;
+
+	EXTI->FTSR |= EXTI_RTSR_TR14;
+	EXTI->IMR |= EXTI_IMR_MR14;
+
+	EXTI->FTSR |= EXTI_RTSR_TR15;
+	EXTI->IMR |= EXTI_IMR_MR15;
 
 	NVIC_EnableIRQ(EXTI15_10_IRQn);			//Разрешаем прерывание
 	NVIC_SetPriority(EXTI15_10_IRQn, 0);	//Выставляем приоритет
 }
-
 
 int main(void)
 {
@@ -198,25 +248,56 @@ int main(void)
 	initTIM3_PWM();
 	initUSART2();
 	initGPIO();
+
     /* Loop forever */
 	while(1)
 	{
+		if (ComReceived == true)
+		{
+			ExecuteCommand();
+		}
+		if (rotation > MAX_ROTATION)
+			stopWindow();
 	}
 }
 
 void EXTI15_10_IRQHandler(void)
 {
-	if (EXTI->PR & EXTI_PR_PR11) 		// нас интересует EXTI14
+	if (EXTI->PR & EXTI_PR_PR11)
 	{
+		rotation = 0;
+		stopWindow();
 		EXTI->PR |= EXTI_PR_PR11;
 	}
-	if (EXTI->PR & EXTI_PR_PR14) 		// нас интересует EXTI14
+	if (EXTI->PR & EXTI_PR_PR13)
 	{
+		rotation = 0;
+		stopWindow();
+		EXTI->PR |= EXTI_PR_PR13;
+	}
+	if (EXTI->PR & EXTI_PR_PR14)
+	{
+		if ((GPIOB->IDR & GPIO_IDR_IDR15) == 0)
+		{
+			rotation += 1;
+		}
+		else
+		{
+			rotation -= 1;
+		}
 		EXTI->PR |= EXTI_PR_PR14;
 	}
-	if (EXTI->PR & EXTI_PR_PR15) 		// нас интересует EXTI15
+	if (EXTI->PR & EXTI_PR_PR15)
 	{
 		EXTI->PR |= EXTI_PR_PR15;
+//		if ((GPIOB->IDR & GPIO_IDR_IDR14) == 0)
+//		{
+//			rotation -= 1;
+//		}
+//		else
+//		{
+//			rotation += 1;
+//		}
 	}
 }
 
